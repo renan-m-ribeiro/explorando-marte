047da39acbf461d7bd9e4cbe66b58cce8c1788e1
renan-m-ribeiro <renan.mendes013@gmail.com>
2022-08-24 18:53:29 -0300

    Create input interface

--- desafio.js
+++ desafio.js
@@ -23,3 +23,61 @@ const robot2 = {
     },
     travel: []
 }
+
+const rl = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+  })
+
+const question1 = () => {
+    return new Promise((resolve, reject) => {
+      rl.question('Lugar > ', (data) => {
+        let position = []
+        data.toString().split(/[\s\n]/,2).forEach(coord => {
+        position.push(parseInt(coord))
+        place.X = position[0]
+        place.Y = position[1]
+        })
+        console.log(place)
+        resolve()
+      })
+    })
+}
+  
+const question2 = () => {
+    return new Promise((resolve, reject) => {
+      rl.question('Posição robo 1 > ', (data) => {
+        let position = []
+        data.toString().split(/[\s\n]/,3).forEach(coord => {
+        position.push(coord.toUpperCase())
+        robot1.position.X = parseInt(position[0])
+        robot1.position.Y = parseInt(position[1])
+        robot1.position.direction = position[2]
+        })
+        console.log(robot1)
+        resolve()
+      })
+    })
+}
+
+const question3 = () => {
+    return new Promise((resolve, reject) => {
+        rl.question('Movimento do robo > ' , (data) => {
+            Array.from(data).forEach(commands => {
+            robot1.travel.push(commands.toUpperCase())
+            })
+            console.log(robot1)
+
+            resolve()
+        })
+    })
+}
+  
+const main = async () => {
+    await question1()
+    await question2()
+    await question3()
+    rl.close()
+}
+  
+main()
