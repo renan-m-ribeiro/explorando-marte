812c8573ac429b1f30d992e9b0ef0faf17f348f5
renan-m-ribeiro <renan.mendes013@gmail.com>
2022-08-24 18:50:44 -0300

    Create array and objects

---
+++ desafio.js
@@ -0,0 +1,25 @@
+const place = { 
+    X: 0, 
+    Y: 0
+ }
+const directions =  [ 'N', 'E', 'S', 'W' ]
+
+const commands = [ 'L', 'R', 'M', 'P' ] 
+
+const robot1 = {
+    position: {
+        X: 0,
+        Y: 0,
+        direction: 'S'
+    },
+    travel: []
+}
+
+const robot2 = {
+    position: {
+        X: 0,
+        Y: 0,
+        direction: ''
+    },
+    travel: []
+}
