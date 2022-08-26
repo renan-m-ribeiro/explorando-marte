c9e153fbfa94ad8f4d4fe2e1e0f7e46c3c80260a
renan-m-ribeiro <renan.mendes013@gmail.com>
2022-08-25 23:51:41 -0300

    Refactoring index.js

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const Robot = require('./Robot.js')
+
 const place = { 
     X: 5, 
     Y: 5,
@@ -6,67 +8,8 @@ const directions =  [ 'N', 'E', 'S', 'W' ]
 
 const commands = [ 'L', 'R', 'M', 'P' ] 
 
-const robot1 = {
-    position: {
-        X: 1,
-        Y: 2,
-        direction: 'N',
-    },
-    travel: [],
-}
-
-const robot2 = {
-    position: {
-        X: 0,
-        Y: 0,
-        direction: '',
-    },
-    travel: [],
-}
-
-function changeDirection(command, robot) {
-    if(command === 'L' || command === 'R') {
-        const directionValue = command === 'L' ? -1 : 1
-        const sumDirection = directions.indexOf(robot.position.direction) + directionValue
-        robot.position.direction = directions.at(sumDirection % directions.length)
-        console.log(`[${robot.position.X}][${robot.position.Y}]-[${robot.position.direction}] --> direction`)
-        return robot.position.direction
-    } else {
-        return console.log('Invalid command.')
-    } 
-}
-
-function move(command, robot) {
-    const { position } = robot
-    if(command === 'M') {
-        robot.position = updatePosition(position)
-        console.log(`[${robot.position.X}][${robot.position.Y}]-[${robot.position.direction}] --> move`)
-        return position
-    } else {
-        return console.log('Invalid command.')
-    }
-}
-
-function updatePosition(position) {
-    const { direction } = position 
-    let newPosition = {...position}
-    
-    if (direction === 'N') {
-        newPosition.Y++
-    } else if (direction === 'S') {
-        newPosition.Y--
-    } else if (direction === 'E') {
-        newPosition.X++
-    } else if (direction === 'W') {
-        newPosition.X--
-    }
-
-    if (newPosition.X < 0 || newPosition.X > place.X || newPosition.Y < 0 || newPosition.Y > place.Y) {
-        return position
-    } else {
-        return newPosition
-    }
-}
+const robot1 = new Robot({ X: 1, Y: 2, direction: 'N'}, [])
+const robot2 = new Robot({ X: 0, Y: 0, direction: 'E'}, [])
 
 const rl = require('readline').createInterface({
     input: process.stdin,
@@ -76,13 +19,6 @@ const rl = require('readline').createInterface({
 const question1 = () => {
     return new Promise((resolve, reject) => {
       rl.question('Lugar > ', (data) => {
-        /*let position = []
-        data.toString().split(/[\s\n]/,2).forEach(coord => {
-            position.push(parseInt(coord))
-            place.X = position[0]
-            place.Y = position[1]
-        })*/
-
         const positions = data.toString().split(/[\s\n]/, 2).map(parseInt)
         place.X = positions[0]
         place.Y = positions[1]
@@ -94,19 +30,10 @@ const question1 = () => {
 const question2 = () => {
     return new Promise((resolve, reject) => {
       rl.question('Posição robo 1 > ', (data) => {
-        /*let position = []
-        data.toString().split(/[\s\n]/,3).forEach(coord => {
-            position.push(coord.toUpperCase())
-            robot1.position.X = parseInt(position[0])
-            robot1.position.Y = parseInt(position[1])
-            robot1.position.direction = position[2]
-        })*/
-
         const positions = data.toString().split(/[\s\n]/, 3)
         robot1.position.X = parseInt(positions[0])
         robot1.position.Y = parseInt(positions[1])
         robot1.position.direction = positions[2].toUpperCase()
-        console.log(robot1)
         resolve()
       })
     })
@@ -115,25 +42,61 @@ const question2 = () => {
 const question3 = () => {
     return new Promise((resolve, reject) => {
         rl.question('Movimento do robo > ' , (data) => {
-            /*Array.from(data).forEach(commands => {
-            robot1.travel.push(commands.toUpperCase())
-            })*/
-
             robot1.travel = [...robot1.travel, ...data.toUpperCase()]
-            console.log(robot1)
+            console.log(robot1.travel)
+            robot1.filterCommands()
             resolve()
         })
     })
 }
+
+const question4 = () => {
+    return new Promise((resolve, reject) => {
+      rl.question('Posição robo 2 > ', (data) => {
+        const positions = data.toString().split(/[\s\n]/, 3)
+        robot2.position.X = parseInt(positions[0])
+        robot2.position.Y = parseInt(positions[1])
+        robot2.position.direction = positions[2].toUpperCase()
+        resolve()
+      })
+    })
+}
+
+const question5 = () => {
+    return new Promise((resolve, reject) => {
+        rl.question('Movimento do robo > ' , (data) => {
+            robot2.travel = [...robot2.travel, ...data.toUpperCase()]
+            console.log(robot2.travel)
+            robot2.filterCommands()
+            resolve()
+        })
+    })
+}
+
+const answer1 = () => {
+    return new Promise((resolve, reject) => {
+        rl.setPrompt(`Posição robo 1 >  ${robot1.position.X} ${robot1.position.Y} ${robot1.position.direction}`)
+            resolve()
+        })
+    }
+
+const answer2 = () => {
+    return new Promise((resolve, reject) => {
+        rl.setPrompt(`Posição robo 1 >  ${robot2.position.X} ${robot2.position.Y} ${robot2.position.direction}`)
+            resolve()
+        })
+    }
   
 const main = async () => {
     await question1()
     await question2()
     await question3()
+    await question4()
+    await question5()
+    await answer1()
+    await answer2()
     rl.close()
 }
 
 main()
 
-
-module.exports = { changeDirection, move }
