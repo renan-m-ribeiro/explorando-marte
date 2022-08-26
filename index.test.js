6bffbdecaed7a10d179c755f6822d480381e735e
renan-m-ribeiro <renan.mendes013@gmail.com>
2022-08-25 23:50:57 -0300

    Update.test.js

--- index.test.js
+++ index.test.js
@@ -1,25 +1,48 @@
-const { move, changeDirection } = require("./index.js");
+const { move, changeDirection, filterCommands } = require("./index.js");
+const Robot = require("./Robot.js");
 
 test('go to 13N', () => {
-    const robot1 = {
-        position: {
-            X: 1,
-            Y: 2,
-            direction: 'N',
-        },
-    }
+    const robot1 = new Robot({ X: 1, Y: 2, direction: 'N'}, [])
 
-    changeDirection('L', robot1)
-    move('M', robot1)
-    changeDirection('L', robot1)
-    move('M', robot1)
-    changeDirection('L', robot1)
-    move('M', robot1)
-    changeDirection('L', robot1)
-    move('M', robot1)
-    move('M', robot1)
+    const commands = ['L', 'M', 'L', 'M', 'L', 'M', 'L', 'M', 'M']
+    robot1.travel = commands
+    robot1.filterCommands()
+    //robot1.position = robot1.travel[robot1.travel.length - 1]
+    //robot1.travel = robot1.commands.map(filterCommands)
+    //robot1.position = robot1.travel[robot1.travel.length - 1]
+    //console.log(robot1.changeDirection('L'))
+    //console.log(robot1.travel)
+
+    
 
     expect(robot1.position.X).toBe(1)
     expect(robot1.position.Y).toBe(3)
     expect(robot1.position.direction).toBe('N')
 })
+
+
+/*test('go to 51E', () => {
+    const robot2 = {
+        position: {
+            X: 3,
+            Y: 3,
+            direction: 'E',
+        },
+    }
+
+    move('M', robot2)
+    move('M', robot2)
+    changeDirection('R', robot2)
+    move('M', robot2)
+    move('M', robot2)
+    changeDirection('R', robot2)
+    move('M', robot2)
+    changeDirection('R', robot2)
+    changeDirection('R', robot2)
+    move('M', robot2)
+
+    expect(robot2.position.X).toBe(5)
+    expect(robot2.position.Y).toBe(1)
+    expect(robot2.position.direction).toBe('E')
+})
+*/
